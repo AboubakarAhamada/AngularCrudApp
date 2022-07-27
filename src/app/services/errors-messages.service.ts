8145f5f716971804355842265b2cfef00d53e8ec
AboubakarAhamada <aboumarshal@hotmail.fr>
2022-07-27 22:33:14 +0100

    Correction de bug : quantity avec virgule

--- src/app/services/errors-messages.service.ts
+++ src/app/services/errors-messages.service.ts
@@ -8,7 +8,7 @@ export class ErrorsMessagesService {
   private fieldRequiredMessage: String = "This field can't be empty.";
   private productNameErrorMessage : String = "Product name must have between 3 and 50 characters.";
   private priceErrorMessage : String = "You cant't set price less than 5 DH."
-  private quantitErrorMessage : String = "The minimal quantity allowed is 1."
+  private quantitErrorMessage : String = "Quantity must be positive integer"
 
   constructor() { }
 
